0cf99015f6ac6ca5c7a16751ac331111100bd8df
BigWaffleMonster <rachkov.daniil2001@gmail.com>
2022-12-11 20:11:35 +0300

    init commit

--- server1/index.js
+++ server1/index.js
@@ -1,9 +1,14 @@
-import express from 'express'
+import express, { json } from 'express'
 
 const app = express()
+app.use(express.json())
 
 app.get('/hello', (req, res) => {
   res.json({ message: 'Hello from server 1' })
+})
+
+app.get('/world', (req, res) => {
+  res.json({ message: 'Hello world from server 1' })
 })
 
 app.listen(3000, () => {
--- server2/index.js
+++ server2/index.js
@@ -1,11 +1,16 @@
-import express from 'express'
+import express, { json } from 'express'
 
 const app = express()
+app.use(express.json())
 
 app.get('/hello', (req, res) => {
   res.json({ message: 'Hello from server 2' })
 })
 
+app.get('/world', (req, res) => {
+  res.json({ message: 'Hello world from server 2' })
+})
+
 app.listen(3001, () => {
-  console.log("Server started on 3001")
+  console.log('Server started on 3001')
 })
